4472eae9eacf7dd560077b505f19dc760674eb33
HauBui <hauviptt@gmail.com>
2019-05-14 14:03:36 +0700

    add-ex-02

--- src/main/java/Exercise02.java
+++ src/main/java/Exercise02.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Exercise02 {
+    public int calTotal(int qty) {
+        int result = 0;
+        while (qty > 0) {
+            result += (qty % 10);
+            qty /= 10;
+        }
+        return result;
+    }
 }
---
+++ src/test/java/Exsercise02Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Exsercise02Test {
+    @Test
+    public void test_calTotal(){
+        assertEquals((new Exercise02()).calTotal(65),11);
+    }
+}
