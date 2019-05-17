e7983b02c32d27c29035827e9486f5f48bfde6a6
HauBui <hauviptt@gmail.com>
2019-05-17 16:14:41 +0700

    exercise 18 - fix

--- src/main/java/Exercise18.java
+++ src/main/java/Exercise18.java
@@ -1,18 +1,36 @@
 public class Exercise18 {
     private String convertDecimalToBinary(int number) {
-        return Integer.toBinaryString(number);
+        String result = "";
+        while (number > 0) {
+            result = (number % 2 == 0) ? "0" + result : "1" + result;
+            number /= 2;
+        }
+        return result;
     }
 
     private int convertBinaryToDecimal(String binaryString) {
-        return Integer.parseInt(reverseString(binaryString), 2);
+        int power = 0;
+        int result = 0;
+        int number = Integer.valueOf(binaryString);
+
+        while (number > 0) {
+            result += (number % 10) * Math.pow(2, power);
+            number /= 10;
+            power++;
+        }
+
+        return result;
     }
 
     private String reverseString(String input) {
-        StringBuilder binaryStringReverse = new StringBuilder(input);
-        return binaryStringReverse.reverse().toString();
+        String result = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            result += input.charAt(i);
+        }
+        return result;
     }
 
     public int convertNumber(int number) {
-        return convertBinaryToDecimal(convertDecimalToBinary(number));
+        return convertBinaryToDecimal(reverseString(convertDecimalToBinary(number)));
     }
 }
