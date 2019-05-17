12d593e7d47af750a556e8baf2c609f958f2413f
HauBui <hauviptt@gmail.com>
2019-05-17 15:25:36 +0700

    exercise 17 - fix

--- src/main/java/Exercise17.java
+++ src/main/java/Exercise17.java
@@ -3,28 +3,24 @@ import java.util.List;
 
 public class Exercise17 {
 
-    private int sumOfDigits(int number) {
-        int count = 0;
+    private int countOfDigits(int number) {
+        int count = 1;
         while (number >= 10) {
             number /= 10; // hay n = n /10;
             count++;
         }
-        return count + 1;
+        return count;
     }
 
     private boolean isValidNumber(int number) {
         int sum = 0, a = number;
-        int index = sumOfDigits(number);
+        int index = countOfDigits(number);
         do {
             int temp = number % 10;
             sum += Math.pow(temp, index);
             number /= 10;
         } while (number > 0);
-        if (sum == a) {
-            return true;
-        } else {
-            return false;
-        }
+        return sum == a;
     }
 
     public List<Integer> displayResult() {
