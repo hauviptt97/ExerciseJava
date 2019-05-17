34c94799a580475b1e62f7218a0b2246f37c145f
HauBui <hauviptt@gmail.com>
2019-05-17 16:58:44 +0700

    exercise 19 - fix

--- src/main/java/Exercise19.java
+++ src/main/java/Exercise19.java
@@ -1,28 +1,21 @@
-import javax.swing.text.ParagraphView;
-import java.math.BigInteger;
-
 public class Exercise19 {
 
-    private BigInteger calculateFactorial(BigInteger number) {
-        return (number.equals(BigInteger.ZERO)) ? BigInteger.ONE : (number.multiply(calculateFactorial(number.subtract(BigInteger.ONE))));
+    private int calculateFactorial(int number) {
+        return (number == 0) ? 1 : (number * calculateFactorial(number - 1));
     }
 
     private int C(int k, int n) {
-        return (calculateFactorial(new BigInteger(n + "")).divide(calculateFactorial(new BigInteger(k + "")).multiply(calculateFactorial(new BigInteger(n - k + ""))))).intValue();
+        return (calculateFactorial(n) / (calculateFactorial(k) * (calculateFactorial(n - k))));
     }
 
-    public String a(int number) {
-        StringBuilder result = new StringBuilder();
+    public int[] a(int number) {
+        int count = 0;
+        int[] result = new int[number];
         for (int i = 0; i < number; i++) {
-            for (int j = 0; j <= number - i; j++) {
-                result.append("  ");
+            for (int j = number - 1; j >= 0; j--) {
+                result[count++] = C(j, i);
             }
-            for (int j = 0; j <= i; j++) {
-                result.append("   " + C(j, i));
-            }
-            result.append("\n");
         }
-        String[] lineData = result.toString().split("\n");
-        return lineData[lineData.length-1].trim();
+        return result;
     }
 }
--- src/test/java/Exercise19Test.java
+++ src/test/java/Exercise19Test.java
@@ -4,7 +4,7 @@ import static junit.framework.TestCase.assertEquals;
 
 public class Exercise19Test {
     @Test
-    public void test_calculateTheSumOfExpression(){
-        assertEquals((new Exercise19()).a(6),"1   5   10   10   5   1");
+    public void test_calculateTheSumOfExpression() {
+        assertEquals((new Exercise19()).a(6), new int[]{1, 5, 10, 10, 5, 1});
     }
 }
