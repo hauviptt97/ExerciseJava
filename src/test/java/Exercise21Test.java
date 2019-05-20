3bf3ac447a162f10722f6b01af32ea9e94a463d5
HauBui <hauviptt@gmail.com>
2019-05-20 10:55:03 +0700

    exercise 21

---
+++ src/test/java/Exercise21Test.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Exercise21Test {
+    @Test
+    public void test_getDaysLeftInMonth(){
+        assertEquals((new Exercise21()).getDaysLeftInMonth("20/05/2019"),11);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testInvalidDate() {
+        assertEquals((new Exercise21()).getDaysLeftInMonth("2005/2019"),0);
+    }
+}
