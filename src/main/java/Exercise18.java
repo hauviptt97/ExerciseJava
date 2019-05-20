6ca0effcc7e4d1eae518ad3462d2a0e7633e38c6
HauBui <hauviptt@gmail.com>
2019-05-20 10:17:41 +0700

    exercise 18 - final

--- src/main/java/Exercise18.java
+++ src/main/java/Exercise18.java
@@ -2,28 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise18 {
-    private List convertDecimalToBinary(int number) {
-        List<Integer> digits = new ArrayList<>();
-        while (number > 0) {
-            digits.add((number % 2 == 0) ? 0 : 1);
-            number /= 2;
-        }
-        return digits;
-    }
-
-    private int convertBinaryToDecimal(List<Integer> binaryDigits) {
-        int power = binaryDigits.size() - 1;
-        int result = 0;
-        for (int index = 0; index < binaryDigits.size(); index++) {
-            result += binaryDigits.get(index) * Math.pow(2, power);
-            power--;
-        }
-
-        return result;
-    }
-
     public int convertNumber(int number) {
-        List<Integer> binaryDigits = convertDecimalToBinary(number);
-        return convertBinaryToDecimal(binaryDigits);
+        int result = 0;
+        while (number != 0) {
+            result <<= 1;
+            result |= number & 1;
+            number >>= 1;
+
+        }
+        return result;
     }
 }
