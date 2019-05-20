1c0266e2c8a1b49d3cd5ecc53d21ed01139757a6
HauBui <hauviptt@gmail.com>
2019-05-20 15:20:49 +0700

    exercise 25

---
+++ src/test/java/Exercise25Test.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Exercise25Test {
+    @Test
+    public void test_countFibonacciNumbers() {
+        Assert.assertEquals(new Exercise25().countFibonacciNumbers(1,5), 5);
+        Assert.assertEquals(new Exercise25().countFibonacciNumbers(2,14), 5);
+    }
+}
