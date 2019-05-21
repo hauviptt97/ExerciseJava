a0862d1a1a7f970ac337698acf752a6c903b33f8
HauBui <hauviptt@gmail.com>
2019-05-21 10:37:11 +0700

    exercise-27-fix

--- src/main/java/Exercise27.java
+++ src/main/java/Exercise27.java
@@ -1,20 +1,10 @@
-import java.math.BigInteger;
 
 public class Exercise27 {
-    private BigInteger calculateFactorial(BigInteger number) {
-        return (number.equals(BigInteger.ZERO)) ? BigInteger.ONE : (number.multiply(calculateFactorial(number.subtract(BigInteger.ONE))));
-    }
-
     public int countZeroInNumber(int number) {
         int count = 0;
-        BigInteger factorial = calculateFactorial(new BigInteger(number + ""));
-        while (factorial.compareTo(BigInteger.ZERO) > 0) {
-            if (factorial.divideAndRemainder(BigInteger.TEN)[1].compareTo(BigInteger.ZERO) == 0) {
-                count++;
-            }
-            factorial = factorial.divide(BigInteger.TEN);
+        for (int i = 5; i <= number; i *= 5) {
+            count += number / i;
         }
         return count;
-
     }
 }
--- src/test/java/Exercise27Test.java
+++ src/test/java/Exercise27Test.java
@@ -4,8 +4,9 @@ import org.junit.Test;
 public class Exercise27Test {
     @Test
     public void test_countZeroInNumber() {
+        Assert.assertEquals(new Exercise27().countZeroInNumber(100), 24);
         Assert.assertEquals(new Exercise27().countZeroInNumber(10), 2);
-        Assert.assertEquals(new Exercise27().countZeroInNumber(12), 4);
+        Assert.assertEquals(new Exercise27().countZeroInNumber(13), 2);
 
     }
 }
