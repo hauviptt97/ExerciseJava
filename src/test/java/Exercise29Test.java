c52f20d798d811a90f64065cfc793cb159df912f
HauBui <hauviptt@gmail.com>
2019-05-21 15:47:42 +0700

    exercise-29-v3

--- src/test/java/Exercise29Test.java
+++ src/test/java/Exercise29Test.java
@@ -8,5 +8,6 @@ public class Exercise29Test {
         assertEquals(new Exercise29().getNumberByRomanLetter("DCLXXX"), 680);
         assertEquals(new Exercise29().getNumberByRomanLetter("DCXXXV"), 635);
         assertEquals(new Exercise29().getNumberByRomanLetter("LXXXIX"), 89);
+        assertEquals(new Exercise29().getNumberByRomanLetter("LXXXXXIX"), 0);
     }
 }
