86c533b02de2791f08f3f3cbf20db238a95c23ab
HauBui <hauviptt@gmail.com>
2019-05-21 09:25:08 +0700

    exercise-19-fix

--- src/main/java/Exercise19.java
+++ src/main/java/Exercise19.java
@@ -2,16 +2,19 @@ import java.util.Arrays;
 
 public class Exercise19 {
 
-    private int calculateFactorial(int number) {
+    private long calculateFactorial(int number) {
         return (number == 0) ? 1 : (number * calculateFactorial(number - 1));
     }
 
-    private int C(int k, int n) {
-        return (calculateFactorial(n) / (calculateFactorial(k) * (calculateFactorial(n - k))));
+    private long C(int k, int n) {
+        long x1= calculateFactorial(n);
+        long x2 = calculateFactorial(k);
+        long x3 = calculateFactorial(n - k);
+teFactorial(n) / (calculateFactorial(k) * (calculateFactorial(n - k))));
     }
 
-    public int[] getLastLineInPascalTriangle(int number) {
-        int[] result = new int[number];
+    public long[] getLastLineInPascalTriangle(int number) {
+        long[] result = new long[number];
         result[0] = 1;
         for (int n = 0; n < number; n++) {
             for (int k = 1; k <= n; k++)
--- src/test/java/Exercise19Test.java
+++ src/test/java/Exercise19Test.java
@@ -1,11 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
-
 public class Exercise19Test {
     @Test
     public void test_calculateTheSumOfExpression() {
-        Assert.assertArrayEquals((new Exercise19()).getLastLineInPascalTriangle(6), new int[]{1, 5, 10, 10, 5, 1});
+        Assert.assertArrayEquals((new Exercise19()).getLastLineInPascalTriangle(100), new long[]{1, 5, 10, 10, 5, 1});
     }
 }
