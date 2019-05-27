65d5cda06301ecf76832e2b264416b571c1f2fdc
HauBui <hauviptt@gmail.com>
2019-05-27 18:16:56 +0700

    exercise 14

--- src/test/java/Exercise14Test.java
+++ src/test/java/Exercise14Test.java
@@ -4,7 +4,8 @@ import static junit.framework.TestCase.assertEquals;
 
 public class Exercise14Test {
     @Test
-    public void test_calculatePi(){
-        assertEquals((new Exercise10()).calculatePi(),3.1415842726746206);
+    public void test_sqrt(){
+        assertEquals((new Exercise14()).sqrt(3),1.7320508100147274);
+        assertEquals((new Exercise14()).sqrt(-3),0.0);
     }
 }
