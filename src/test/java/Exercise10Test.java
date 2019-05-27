24a8d5f59793edf159f74015dc21ea3815bb8e79
HauBui <hauviptt@gmail.com>
2019-05-27 13:49:40 +0700

    exercise 10

--- src/test/java/Exercise10Test.java
+++ src/test/java/Exercise10Test.java
@@ -5,6 +5,6 @@ import static junit.framework.TestCase.assertEquals;
 public class Exercise10Test {
     @Test
     public void test_calculatePi(){
-        assertEquals((new Exercise10()).calculatePi(),3.1414926535900345);
+        assertEquals((new Exercise10()).calculatePi(),3.1415842726746206);
     }
 }
