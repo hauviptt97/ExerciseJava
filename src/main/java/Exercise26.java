f73f99ac3058527de259341f42ef3818d245d562
HauBui <hauviptt@gmail.com>
2019-05-28 10:32:02 +0700

    exercise 26

--- src/main/java/Exercise26.java
+++ src/main/java/Exercise26.java
@@ -1,5 +1,18 @@
 public class Exercise26 {
-    public int[] getCrazyFibonacci(int a0, int a1, int a2, int a3, int a4) {
-                
+    public int getCrazyFibonacci(int[] a, int n) {
+        if (n <= 5 && n > 0) return a[n - 1];
+
+        if (n < 1) return 0;
+
+        int a5 = 0;
+        for (int i = 6; i <= n; i++) {
+            a5 = (a[0] + a[1] + a[2] + a[3] + a[4]) % 10;
+            a[0] = a[1];
+            a[1] = a[2];
+            a[2] = a[3];
+            a[3] = a[4];
+            a[4] = a5;
+        }
+        return a5;
     }
 }
--- src/test/java/Exercise26Test.java
+++ src/test/java/Exercise26Test.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class Exercise26Test {
+    @Test
+    public void test_calculateTheSumOfExpression(){
+        assertEquals((new Exercise26()).getCrazyFibonacci(new int[]{6,2,2,1,4},7),4);
+        assertEquals((new Exercise26()).getCrazyFibonacci(new int[]{6,2,2,1,4},6),5);
+        assertEquals((new Exercise26()).getCrazyFibonacci(new int[]{6,2,2,1,4},4),1);
+        assertEquals((new Exercise26()).getCrazyFibonacci(new int[]{6,2,2,1,4},0),0);
+    }
 }
