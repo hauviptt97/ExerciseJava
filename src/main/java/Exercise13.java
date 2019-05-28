8fbfed4ae9e7a70c1eaf3a86bb11f84a3ff566a7
HauBui <hauviptt@gmail.com>
2019-05-28 09:39:16 +0700

    exercise 13 - fix

--- src/main/java/Exercise13.java
+++ src/main/java/Exercise13.java
@@ -3,14 +3,13 @@ public class Exercise13 {
     public double calculateE() {
         int k = 1;
         double currentValue = 1;
-        double previousValue = 0;
+        double sum = currentValue;
         double epsilon = 0.00001;
-        while (Math.abs(currentValue - previousValue) > epsilon) {
-            previousValue = currentValue;
-            currentValue += previousValue / k;
-            System.out.println(currentValue);
+        while (currentValue > epsilon) {
+            currentValue = currentValue / k;
+            sum += currentValue;
             k++;
         }
-        return currentValue;
+        return sum;
     }
 }
--- src/test/java/Exercise13Test.java
+++ src/test/java/Exercise13Test.java
@@ -5,6 +5,6 @@ import static junit.framework.TestCase.assertEquals;
 public class Exercise13Test {
     @Test
     public void test_calculateE(){
-        assertEquals((new Exercise13()).calculateE(),2.71827876984127);
+        assertEquals((new Exercise13()).calculateE(),2.7182815255731922);
     }
 }
